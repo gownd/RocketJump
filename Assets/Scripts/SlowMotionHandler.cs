5dc0e13e066bc8888809f84beff0f3a494e3f168
HJ <qsdaw753@naver.com>
2022-01-10 11:58:05 +0900

    데모 레벨 디자인 다듬기

--- Assets/Scripts/SlowMotionHandler.cs
+++ Assets/Scripts/SlowMotionHandler.cs
@@ -7,6 +7,11 @@ public class SlowMotionHandler : MonoBehaviour
 {
     [SerializeField] MMFeedbacks slowMotionFeedback = null;
 
+    private void Start() 
+    {
+        Time.fixedDeltaTime = 0.02f;    
+    }
+
     void Update()
     {
         if(Input.GetMouseButtonDown(1))
